01944b7d2b08b3f6e03b1a5a7c1e92919edf8be9
Priyanshu Raj <rpriyanshu9@gmail.com>
2021-06-21 11:49:06 +0530

    added 0-1 knapsack

---
+++ 2-d_array_problems/0_1_knapsack.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    //Function to return max value that can be put in knapsack of capacity W.
+
+    // Recursive
+    // int helper(int index,int W,int wt[],int val[], int n){
+    //     if(index>=n) return 0;
+    //     if(wt[index]<=W){
+    //         return max(val[index]+helper(index+1,W-wt[index],wt,val,n),helper(index+1,W,wt,val,n));
+    //     }else{
+    //         return helper(index+1,W,wt,val,n);
+    //     }
+    // }
+
+    //Memoization
+
+    // vector<vector<int>> memo = vector<vector<int>>(1001,vector<int>(1001,-1));
+
+    // int helper(int index,int W,int wt[],int val[], int n){
+    //     if(index>=n) return 0;
+    //     if(memo[index][W]!=-1) return memo[index][W];
+    //     if(wt[index]<=W){
+    //         return memo[index][W]=max(val[index]+helper(index+1,W-wt[index],wt,val,n),helper(index+1,W,wt,val,n));
+    //     }else{
+    //         return memo[index][W] =helper(index+1,W,wt,val,n);
+    //     }
+    // }
+
+    // Iterative
+    int knapSack(int W, int wt[], int val[], int n)
+    {
+        int dp[n + 1][W + 1];
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int j = 0; j < W + 1; j++)
+            {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else if (wt[i - 1] <= j)
+                {
+                    dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][W];
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    //taking total testcases
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin >> n >> w;
+
+        int val[n];
+        int wt[n];
+
+        //inserting the values
+        for (int i = 0; i < n; i++)
+            cin >> val[i];
+
+        //inserting the weights
+        for (int i = 0; i < n; i++)
+            cin >> wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout << ob.knapSack(w, wt, val, n) << endl;
+    }
+    return 0;
+} // } Driver Code Ends
